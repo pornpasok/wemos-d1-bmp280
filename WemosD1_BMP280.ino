0c53217e18cb7d4eddc1b39a420eba024d2b4e6b
Administrator <admin@busx.com>
2020-06-28 21:33:00 +0700

    Add WemosD1_BMP280.ino

---
+++ WemosD1_BMP280.ino
@@ -0,0 +1,51 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_BMP280.h>
+
+#define BMP_SCK  (13)
+#define BMP_MISO (12)
+#define BMP_MOSI (11)
+#define BMP_CS   (10)
+
+ADC_MODE(ADC_VCC);
+float v;
+
+Adafruit_BMP280 bmp; // I2C
+//Adafruit_BMP280 bmp(BMP_CS); // hardware SPI
+//Adafruit_BMP280 bmp(BMP_CS, BMP_MOSI, BMP_MISO,  BMP_SCK);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("BMP280 test"));
+  
+  bool status = bmp.begin(0x76);
+  if (!status) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring or change I2C address!");
+    while (1);
+  }
+}
+
+void loop() {
+    // VCC
+    v = ESP.getVcc() / 1000.0;
+    Serial.print("VCC: ");
+    Serial.println(v);// VCC
+  
+    Serial.print(F("Temperature = "));
+    Serial.print(bmp.readTemperature());
+    Serial.println(" *C");
+
+    Serial.print(F("Pressure = "));
+    Serial.print(bmp.readPressure()/100.0F);
+    Serial.println(" hPa");
+
+    Serial.print(F("Approx altitude = "));
+    Serial.print(bmp.readAltitude(1013.25)); /* Adjusted to local forecast! */
+    Serial.println(" m");
+
+    Serial.print("Chip ID: ");
+    Serial.println(ESP.getFlashChipId());
+
+    Serial.println();
+    delay(2000);
+}
